72d821e514b32e01022eeb74df8c2d5032041576
Omkar Ekale <omkarekale31@gmail.com>
2024-11-29 14:20:33 +0530

    second repo

--- index.js
+++ index.js
@@ -7,22 +7,23 @@ import tweetRoute from "./routes/tweetRoute.js";
 import cors from "cors";
 
 dotenv.config({
-    path:".env"
+    path: ".env"
 })
 databaseConnection();
-const app = express(); 
+const app = express();
 
 // middlewares
 app.use(express.urlencoded({
-    extended:true
+    extended: true
 }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
     origin: [
         // "https://twitter-clone-3-txso.onrender.com",
+        "https://comforting-pony-58050e.netlify.app"
         // "http://localhost:3000"
-        "http://localhost:3000", 
+        // "http://localhost:3000", 
     ],
     credentials: true
 }
@@ -32,10 +33,10 @@ app.get('/', (req, res) => {
     res.send('Welcome to the API');
 });
 // api
-app.use("/api/v1/user",userRoute);
+app.use("/api/v1/user", userRoute);
 app.use("/api/v1/tweet", tweetRoute);
- 
 
-app.listen(process.env.PORT,() => {
+
+app.listen(process.env.PORT, () => {
     console.log(`Server listen at port ${process.env.PORT}`);
 })
